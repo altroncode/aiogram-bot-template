3fd86c1bd968e61167245dfc0abb7a55630ca7bd
altcode <cosmosx1328@gmail.com>
2022-08-03 12:36:39 +0600

    :recycle: Rename bot_start to start

--- src/handlers/users/start.py
+++ src/handlers/users/start.py
@@ -1,10 +1,10 @@
 from aiogram.dispatcher import filters
 import aiogram.types
 
-from responses import start
+import responses.start
 from loader import dp
 
 
 @dp.message_handler(filters.CommandStart())
-async def bot_start(message: aiogram.types.Message):
-    await start.StartResponse(message)
+async def start(message: aiogram.types.Message):
+    await responses.start.StartResponse(message)
